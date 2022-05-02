702de56f28151d10427a85bc03234b838039d7b5
Qian QIAO <q.qiao@dalloz.fr>
2022-05-02 22:39:35 +0200

    Add the readme

--- target/classes/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO STOCK (id, name, shoes, capacity) VALUES (1, 'sport', '[{"color": "BLACK","size": 40,"quantity": 10},{"color": "BLACK","size": 41,"quantity": 0},{"color": "BLUE","size": 39,"quantity": 10}]', 30);
